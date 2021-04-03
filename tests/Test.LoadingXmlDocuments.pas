489f15f2e2be9e9c04f4e1595c585feba022fc96
Jolyon Direnko-Smith <jsmith@deltics.co.nz>
2021-04-04 12:03:15 +1300

    test: Initial tests

---
+++ tests/Test.LoadingXmlDocuments.pas
@@ -0,0 +1,107 @@
+
+{$i deltics.inc}
+
+  unit Test.LoadingXmlDocuments;
+
+interface
+
+  uses
+    Deltics.Smoketest;
+
+
+  type
+    LoadingXmlDocuments = class(TTest)
+      procedure NoteXml;
+      procedure EmptyElement;
+      procedure EmptyElementWithAttributes;
+      procedure ElementWithChild;
+    end;
+
+
+implementation
+
+  uses
+    Deltics.StringLists,
+    Deltics.Xml;
+
+
+{ XmlDocument }
+
+  var
+    sut: IXmlDocument;
+
+  procedure LoadingXmlDocuments.NoteXml;
+  var
+    note: IXmlElement;
+  begin
+    // TODO: Support enfironment variables in filepath
+    Xml.Load(sut).FromFile('X:\dev\src\delphi\libs\congress\deltics.xml\tests\samples\note.xml');
+
+    Test('Document').Assert(sut).IsAssigned;
+    Test('Document.DocType').Assert(sut.DocType).IsNIL;
+    Test('Document.RootElement').Assert(sut.RootElement).IsAssigned;
+    Test('Document.Nodes').Assert(sut.Nodes).IsAssigned;
+    Test('Document.Nodes.Count').Assert(sut.Nodes.Count).Equals(2);
+
+    note := sut.RootElement;
+
+    Test('note.Nodes.Count').Assert(note.Nodes.Count).Equals(4);
+    Test('note.Nodes[0].Name').AssertUtf8(note.Nodes[0].Name).Equals('to');
+    Test('note.Nodes[1].Name').AssertUtf8(note.Nodes[1].Name).Equals('from');
+    Test('note.Nodes[2].Name').AssertUtf8(note.Nodes[2].Name).Equals('heading');
+    Test('note.Nodes[3].Name').AssertUtf8(note.Nodes[3].Name).Equals('body');
+
+    Test('note.Nodes[0].Text').AssertUtf8(note.Nodes[0].Text).Equals('Tove');
+    Test('note.Nodes[1].Text').AssertUtf8(note.Nodes[1].Text).Equals('Jani');
+    Test('note.Nodes[2].Text').AssertUtf8(note.Nodes[2].Text).Equals('Reminder');
+    Test('note.Nodes[3].Text').AssertUtf8(note.Nodes[3].Text).Equals('Don''t forget me this weekend!');
+  end;
+
+
+  procedure LoadingXmlDocuments.ElementWithChild;
+  begin
+    Xml.Load(sut).FromString('<root><child /></root>');
+
+    Test('Nodes.Count').Assert(sut.Nodes.Count).Equals(1);
+
+    Test('RootElement').Assert(sut.RootElement).IsAssigned;
+    Test('RootElement.IsEmpty').Assert(sut.RootElement.IsEmpty).IsFalse;
+    Test('RootElement.Name').Assertutf8(sut.RootElement.Name).Equals('root');
+
+    Test('RootElement.Nodes').Assert(sut.RootElement.Nodes).IsAssigned;
+    Test('RootElement.Nodes.Count').Assert(sut.RootElement.Nodes.Count).Equals(1);
+    Test('RootElement.Nodes[0].Name').Assertutf8(sut.RootElement.Nodes[0].Name).Equals('child');
+  end;
+
+
+  procedure LoadingXmlDocuments.EmptyElement;
+  begin
+    Xml.Load(sut).FromString('<root />');
+
+    Test('Nodes.Count').Assert(sut.Nodes.Count).Equals(1);
+    Test('RootElement').Assert(sut.RootElement).IsAssigned;
+    Test('RootElement.IsEmpty').Assert(sut.RootElement.IsEmpty).IsTrue;
+    Test('RootElement.Name').Assertutf8(sut.RootElement.Name).Equals('root');
+  end;
+
+
+  procedure LoadingXmlDocuments.EmptyElementWithAttributes;
+  begin
+    Xml.Load(sut).FromString('<root attr1="first" attr2="second" />');
+
+    Test('Nodes.Count').Assert(sut.Nodes.Count).Equals(1);
+
+    Test('RootElement').Assert(sut.RootElement).IsAssigned;
+    Test('RootElement.IsEmpty').Assert(sut.RootElement.IsEmpty).IsTrue;
+    Test('RootElement.Name').Assertutf8(sut.RootElement.Name).Equals('root');
+
+    Test('RootElement.Attributes.Count').Assert(sut.RootElement.Attributes.Count).Equals(2);
+    Test('RootElement.Attributes[0].Name').AssertUtf8(sut.RootElement.Attributes[0].Name).Equals('attr1');
+    Test('RootElement.Attributes[0].Value').AssertUtf8(sut.RootElement.Attributes[0].Value).Equals('first');
+    Test('RootElement.Attributes[1].Name').AssertUtf8(sut.RootElement.Attributes[1].Name).Equals('attr2');
+    Test('RootElement.Attributes[1].Value').AssertUtf8(sut.RootElement.Attributes[1].Value).Equals('second');
+  end;
+
+
+
+end.
--- tests/test.dpr
+++ tests/test.dpr
@@ -9,15 +9,41 @@ uses
   FastMM4,
   Deltics.Smoketest,
   Deltics.Xml in '..\src\Deltics.Xml.pas',
-  Deltics.Xml.Dtd in '..\src\Deltics.Xml.Dtd.pas',
+  Deltics.Xml.Exceptions in '..\src\Deltics.Xml.Exceptions.pas',
+  Deltics.Xml.Factories in '..\src\Deltics.Xml.Factories.pas',
+  Deltics.Xml.Fpi in '..\src\Deltics.Xml.Fpi.pas',
+  Deltics.Xml.Interfaces in '..\src\Deltics.Xml.Interfaces.pas',
+  Deltics.Xml.Loader in '..\src\Deltics.Xml.Loader.pas',
+  Deltics.Xml.Nodes in '..\src\Deltics.Xml.Nodes.pas',
+  Deltics.Xml.Nodes.Attributes in '..\src\Deltics.Xml.Nodes.Attributes.pas',
+  Deltics.Xml.Nodes.Attributes.Namespaces in '..\src\Deltics.Xml.Nodes.Attributes.Namespaces.pas',
+  Deltics.Xml.Nodes.CDATA in '..\src\Deltics.Xml.Nodes.CDATA.pas',
+  Deltics.Xml.Nodes.Comment in '..\src\Deltics.Xml.Nodes.Comment.pas',
+  Deltics.Xml.Nodes.DocType in '..\src\Deltics.Xml.Nodes.DocType.pas',
+  Deltics.Xml.Nodes.Document in '..\src\Deltics.Xml.Nodes.Document.pas',
+  Deltics.Xml.Nodes.Dtd in '..\src\Deltics.Xml.Nodes.Dtd.pas',
+  Deltics.Xml.Nodes.Dtd.Attributes in '..\src\Deltics.Xml.Nodes.Dtd.Attributes.pas',
+  Deltics.Xml.Nodes.Dtd.ContentParticles in '..\src\Deltics.Xml.Nodes.Dtd.ContentParticles.pas',
+  Deltics.Xml.Nodes.Dtd.Elements in '..\src\Deltics.Xml.Nodes.Dtd.Elements.pas',
+  Deltics.Xml.Nodes.Dtd.Entities in '..\src\Deltics.Xml.Nodes.Dtd.Entities.pas',
+  Deltics.Xml.Nodes.Dtd.Notation in '..\src\Deltics.Xml.Nodes.Dtd.Notation.pas',
+  Deltics.Xml.Nodes.Elements in '..\src\Deltics.Xml.Nodes.Elements.pas',
+  Deltics.Xml.Nodes.Fragment in '..\src\Deltics.Xml.Nodes.Fragment.pas',
+  Deltics.Xml.Nodes.ProcessingInstruction in '..\src\Deltics.Xml.Nodes.ProcessingInstruction.pas',
+  Deltics.Xml.Nodes.Prolog in '..\src\Deltics.Xml.Nodes.Prolog.pas',
+  Deltics.Xml.Nodes.Text in '..\src\Deltics.Xml.Nodes.Text.pas',
   Deltics.Xml.Parser in '..\src\Deltics.Xml.Parser.pas',
   Deltics.Xml.Reader in '..\src\Deltics.Xml.Reader.pas',
-  Deltics.Xml.Schema in '..\src\Deltics.Xml.Schema.pas',
-  Deltics.Xml.Schema.Consts in '..\src\Deltics.Xml.Schema.Consts.pas',
-  Deltics.Xml.Schema.Reader in '..\src\Deltics.Xml.Schema.Reader.pas',
-  Deltics.Xml.Schema.Writer in '..\src\Deltics.Xml.Schema.Writer.pas',
-  Deltics.Xml.Selections in '..\src\Deltics.Xml.Selections.pas',
-  Deltics.Xml.Writer in '..\src\Deltics.Xml.Writer.pas';
+  Deltics.Xml.Types in '..\src\Deltics.Xml.Types.pas',
+  Deltics.Xml.Utils in '..\src\Deltics.Xml.Utils.pas',
+  Deltics.Xml.Writer in '..\src\Deltics.Xml.Writer.pas',
+  Test.BuildingXmlDocuments in 'Test.BuildingXmlDocuments.pas',
+  Test.LoadingXmlDocuments in 'Test.LoadingXmlDocuments.pas',
+  Test.FormattingXml in 'Test.FormattingXml.pas',
+  Deltics.Xml.Formatter in '..\src\Deltics.Xml.Formatter.pas';
 
 begin
+  TestRun.Test(BuildingXmlDocuments);
+  TestRun.Test(LoadingXmlDocuments);
+  TestRun.Test(FormattingXml);
 end.
