f539e38d74ec5ff46fcc1205ad7abcd3882b4d7c
Jolyon Direnko-Smith <jsmith@deltics.co.nz>
2021-04-05 09:29:12 +1200

    refactor: Removed factory classes, replaced with Xml factory methods

--- src/Deltics.Xml.pas
+++ src/Deltics.Xml.pas
@@ -7,9 +7,11 @@
 interface
 
   uses
-    Deltics.Xml.Factories,
     Deltics.Xml.Interfaces,
-    Deltics.Xml.Types;
+    Deltics.Xml.Types,
+    Deltics.Xml.Nodes.Attributes,
+    Deltics.Xml.Nodes.Document,
+    Deltics.Xml.Nodes.Elements;
 
 
   type
@@ -19,7 +21,7 @@ interface
     TXmlDtdContentParticleListType = Deltics.Xml.Types.TXmlDtdContentParticleListType;
     TXmlDtdAttributeType           = Deltics.Xml.Types.TXmlDtdAttributeType;
     TXmlDtdAttributeConstraint     = Deltics.Xml.Types.TXmlDtdAttributeConstraint;
-    TXmlFormatterLineEndings       = Deltics.Xml.Types.TXmlFormatterLineEndings;
+    TXmlLineEndings                = Deltics.Xml.Types.TXmlLineEndings;
 
 
   const
@@ -52,7 +54,7 @@ interface
     ecChildren               = Deltics.Xml.Types.ecChildren;
 
     xmlUndefined             = Deltics.Xml.Types.xmlUndefined;
-//    xmlElement               = Deltics.Xml.Types.xmlElement;
+    xmlElement               = Deltics.Xml.Types.xmlElement;
     xmlAttribute             = Deltics.Xml.Types.xmlAttribute;
     xmlText                  = Deltics.Xml.Types.xmlText;
     xmlCDATA                 = Deltics.Xml.Types.xmlCDATA;
@@ -60,7 +62,7 @@ interface
     xmlEntity                = Deltics.Xml.Types.xmlEntity;
     xmlProcessingInstruction = Deltics.Xml.Types.xmlProcessingInstruction;
     xmlComment               = Deltics.Xml.Types.xmlComment;
-//    xmlDocument              = Deltics.Xml.Types.xmlDocument;
+    xmlDocument              = Deltics.Xml.Types.xmlDocument;
     xmlDocType               = Deltics.Xml.Types.xmlDocType;
     xmlFragment              = Deltics.Xml.Types.xmlFragment;
 
@@ -72,10 +74,10 @@ interface
     xmlDtdElement             = Deltics.Xml.Types.xmlDtdElement;
     xmlDtdEntity              = Deltics.Xml.Types.xmlDtdEntity;
     xmlDtdNotation            = Deltics.Xml.Types.xmlDtdNotation;
-    xmlEndTag                 = Deltics.Xml.Types.xmlEndTag;
-    xmlNamespace              = Deltics.Xml.Types.xmlNamespace;
-    xmlProlog                 = Deltics.Xml.Types.xmlProlog;
-    xmlWhitespace             = Deltics.Xml.Types.xmlWhitespace;
+    xmlEndTag                 = Deltics.Xml.Types.xmlEndTag;      // Transient node (discarded after detection) used to simplify loading of Xml
+    xmlNamespace              = Deltics.Xml.Types.xmlNamespace;   // Special case of xmlAttribute (xmlns or xmls:<prefix>)
+    xmlProlog                 = Deltics.Xml.Types.xmlProlog;      // Special case of xmlProcessingInstruction
+    xmlWhitespace             = Deltics.Xml.Types.xmlWhitespace;  // Intended for whitespace preservation.  Not currently used!
 
     xmlLF                     = Deltics.Xml.Types.xmlLF;
     xmlCRLF                   = Deltics.Xml.Types.xmlCRLF;
@@ -94,10 +96,13 @@ interface
     IXmlElementSelection    = Deltics.Xml.Interfaces.IXmlElementSelection;
     IXmlNamespaceSelection  = Deltics.Xml.Interfaces.IXmlNamespaceSelection;
 
-  // Factories
+
+  // Implementation classes for use where needed.  Recommend use of
+  //  Xml factory methods where possible
   type
-    XmlDocument = Deltics.Xml.Factories.XmlDocument;
-    XmlElement  = Deltics.Xml.Factories.XmlElement;
+    TXmlAttribute = Deltics.Xml.Nodes.Attributes.TXmlAttribute;
+    TXmlDocument  = Deltics.Xml.Nodes.Document.TXmlDocument;
+    TXmlElement   = Deltics.Xml.Nodes.Elements.TXmlElement;
 
 
   // Utilities
@@ -105,6 +110,12 @@ interface
     Xml = class
       class function Format(const aRootNode: IXmlNode): IXmlFormatter;
       class function Load(var aDocument: IXmlDocument): IXmlLoader;
+
+      // Factory methods
+      class function Attribute(const aName: Utf8String; const aValue: Utf8String = ''): IXmlAttribute;
+      class function Document: IXmlDocument;
+      class function Element(const aName: Utf8String): IXmlElement; overload;
+      class function Element(const aName: Utf8String; const aText: Utf8String): IXmlElement; overload;
     end;
 
 
@@ -116,14 +127,44 @@ implementation
     Deltics.Xml.Loader;
 
 
-{ Xml }
+{ Xml -------------------------------------------------------------------------------------------- }
 
+  { - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - }
+  class function Xml.Attribute(const aName, aValue: Utf8String): IXmlAttribute;
+  begin
+    result := TXmlAttribute.Create(aName, aValue);
+  end;
+
+
+  { - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - }
+  class function Xml.Element(const aName: Utf8String): IXmlElement;
+  begin
+    result := TXmlElement.Create(aName);
+  end;
+
+
+  { - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - }
+  class function Xml.Document: IXmlDocument;
+  begin
+    result := TXmlDocument.Create;
+  end;
+
+
+  { - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - }
+  class function Xml.Element(const aName, aText: Utf8String): IXmlElement;
+  begin
+    result := TXmlElement.Create(aName, aText);
+  end;
+
+
+  { - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - }
   class function Xml.Format(const aRootNode: IXmlNode): IXmlFormatter;
   begin
     result := TXmlFormatter.Create(aRootNode);
   end;
 
 
+  { - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - }
   class function Xml.Load(var aDocument: IXmlDocument): IXmlLoader;
   begin
     result := TXmlLoader.Create(aDocument);
